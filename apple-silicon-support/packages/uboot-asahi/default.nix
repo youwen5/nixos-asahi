266699e79ae3188a285cd2d7a7bb6d89e773ca29
Zzy Wysm <zzy@zzywysm.com>
2024-10-09 12:25:14 -0400

    uboot-asahi: config refinement
    * use u-boot release intended for non-Fedora distros
    * decrease the boot delay to speed up overall boot time
    * disable the submarine logo on boot because it looks goofy
    * disable other unneeded code
    
    We remove the 16x32 option because it's now enabled by default by Asahi's patches

--- apple-silicon-support/packages/uboot-asahi/default.nix
+++ apple-silicon-support/packages/uboot-asahi/default.nix
@@ -9,10 +9,10 @@
     # tracking: https://pagure.io/fedora-asahi/uboot-tools/commits/main
     owner = "AsahiLinux";
     repo = "u-boot";
-    rev = "c134629a8bc448e979967bf0632fdd5bb42ee1d7";
-    hash = "sha256-EPdpyvblkvQrMCiPjX3Bnqp8s/3Yd/gYM1PQu4PDMhs=";
+    rev = "asahi-v2024.04-1";
+    hash = "sha256-/cpdNLO83pW9uOKFJgGQdSzNUQuE2x5oLVFeoElcTbs=";
   };
-  version = "2024.04-4-asahi";
+  version = "2024.04-1-asahi";
 
   defconfig = "apple_m1_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
@@ -22,10 +22,26 @@
   ];
   extraConfig = ''
     CONFIG_IDENT_STRING=" ${version}"
+    CONFIG_BOOTMETH_VBE=n
+    CONFIG_AUTOBOOT_KEYED=n
+    CONFIG_ARM64_SUPPORT_AARCH32=n
+    CONFIG_SYS_MALLOC_CLEAR_ON_INIT=n
+    CONFIG_BOOTDELAY=1
+    CONFIG_BOOTM_VXWORKS=n
+    CONFIG_BOOTM_NETBSD=n
+    CONFIG_BOOTM_PLAN9=n
+    CONFIG_BOOTM_RTEMS=n
+    CONFIG_CMD_LZMADEC=n
+    CONFIG_LZMA=n
+    CONFIG_CMD_UNLZ4=n
+    CONFIG_LZ4=n
+    CONFIG_TOOLS_KWBIMAGE=n
+    CONFIG_CMD_SELECT_FONT=n
+    CONFIG_VIDEO_LOGO=n
+    CONFIG_VIDEO_BMP_RLE8=n
     CONFIG_VIDEO_FONT_4X6=n
     CONFIG_VIDEO_FONT_8X16=n
     CONFIG_VIDEO_FONT_SUN12X22=n
-    CONFIG_VIDEO_FONT_16X32=y
   '';
 }).overrideAttrs (o: {
   # nixos's downstream patches are not applicable
