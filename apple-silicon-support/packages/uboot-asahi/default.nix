09d4e26b7d49323faad37264763cf57988cfd720
Zzy Wysm <zzy@zzywysm.com>
2024-11-13 17:24:33 -0500

    uboot-asahi: asahi-v2024.07-1 -> asahi-v2024.10-1

--- apple-silicon-support/packages/uboot-asahi/default.nix
+++ apple-silicon-support/packages/uboot-asahi/default.nix
@@ -9,10 +9,10 @@
     # tracking: https://pagure.io/fedora-asahi/uboot-tools/commits/main
     owner = "AsahiLinux";
     repo = "u-boot";
-    rev = "asahi-v2024.07-1";
-    hash = "sha256-TN82UydeOyt2EywFjAmtXR6x68XfzwMXxqoJyTxOwCw=";
+    rev = "asahi-v2024.10-1";
+    hash = "sha256-gtXt+BglBdEKW7j3U2x2QeKGeDH1FdmAMPXk+ntkROo=";
   };
-  version = "2024.07-1-asahi";
+  version = "2024.10-1-asahi";
 
   defconfig = "apple_m1_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
